c8aab90b0edd97de59d2e1bc91c21d88016447bf
Akanksha Priya <akankshapriya2162000@gmail.com>
2020-12-18 16:01:00 +0530

    added solutions

---
+++ solutions/Q1.cpp
@@ -0,0 +1,41 @@
+//reverse the aarray
+#include<bits/stdc++.h>
+using namespace std;
+
+//method 1
+//using method of swapping of elements from both start and end
+void reverseArray(int arr[], int start, int end)
+{
+	while(start<end)
+	{
+		int temp = arr[start];
+		arr[start] = arr[end];
+		arr[end] = temp;
+		start++;
+	}
+}
+
+void printArray(int arr[], int size)
+{
+	for(int i=0; i<size; i++)
+		cout<<arr[i]<<" ";
+	cout<<endl;
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0; i<n; i++)
+	{
+		cin>>arr[i];
+
+	}
+	cout<<"given array";
+	printArray(arr,n);
+	reverseArray(arr,0,n-1);
+	cout<<"reversed array: ";
+	printArray(arr,n);
+	return 0;
+}
